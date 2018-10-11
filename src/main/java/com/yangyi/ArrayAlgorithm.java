2b1d2d93e8de5e0d50052c031f4d60e956244660
Tinu <kcyangyi@gmail.com>
2018-10-11 10:47:12 +0800

    commit

--- src/main/java/com/yangyi/ArrayAlgorithm.java
+++ src/main/java/com/yangyi/ArrayAlgorithm.java
@@ -125,9 +125,6 @@ public class ArrayAlgorithm {
                     return false;
                 }
                 set.add(arr[j]);
-                if (true) {
-                
-                }
             }
         }
         return true;
