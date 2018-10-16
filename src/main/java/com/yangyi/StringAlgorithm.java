2039fda976e0ff3a5d7dda3bc327e0fde0d2b0b0
Tinu <kcyangyi@gmail.com>
2018-10-16 21:24:22 +0800

    commit

--- src/main/java/com/yangyi/StringAlgorithm.java
+++ src/main/java/com/yangyi/StringAlgorithm.java
@@ -12,4 +12,68 @@ public class StringAlgorithm {
         }
         return temp.toString();
     }
+    
+    /**
+     * 颠倒整数
+     */
+    public int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        String numStr = x + "";
+        
+        String sign = "";
+        
+        String temp = "";
+        
+        String[] arr = numStr.split("");
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (("0").equals(arr[i]) && i == arr.length - 1) {
+                continue;
+            }
+            if (("-").equals(arr[i])) {
+                sign = "-";
+                continue;
+            }
+            temp += arr[i];
+        }
+        
+        temp = sign + temp;
+        
+        int num = 0;
+        try {
+            num = Integer.parseInt(temp);
+        } catch (RuntimeException e) {
+            num = 0;
+        } finally {
+            return num;
+        }
+    }
+    
+    public int firstUniqChar(String s) {
+        if (s.length() == 0 || s.equals("") || s == null) {
+            return -1;
+        }
+        if (s.length() == 1) {
+            return 0;
+        }
+        String[] arr = s.split("");
+        for (int i = 0; i < arr.length; i++) {
+            boolean flag = true;
+            if (i == arr.length - 1) {
+                return -1;
+            }
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].equals(arr[j])) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
